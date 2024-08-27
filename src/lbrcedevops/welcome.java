20f24f7a5321c599c49671c9263f0424fc1873be
AdmiN <AdmiN@ITWS000>
2024-08-27 11:55:02 +0530

    commit

--- src/lbrcedevops/welcome.java
+++ src/lbrcedevops/welcome.java
@@ -5,6 +5,8 @@ public class welcome {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Devops Lab");
 		System.out.println("Welcome");
+		System.out.println("Welcome to Devops Lab");
+		System.out.println("Welcome");
 	}
 
 }
